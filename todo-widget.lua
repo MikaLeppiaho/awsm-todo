56ea3fc43741d6b6d5dd01bf55ed51abc09379a9
Mika Leppiaho <mika.leppiaho@gmail.com>
2024-10-12 18:35:20 +0300

    Added feature to read tasks from file

--- todo-widget.lua
+++ todo-widget.lua
@@ -3,23 +3,9 @@ local wibox = require("wibox")
 local gears = require("gears")
 local beautiful = require("beautiful")
 
---Define the popup widget
+-- Define the popup widget
 local todo_widget = awful.popup({
-	widget = {
-		{
-			{
-				text = "TODO List:",
-				widget = wibox.widget.textbox,
-			},
-			{
-				text = "1. Example task",
-				widget = wibox.widget.textbox,
-			},
-			layout = wibox.layout.fixed.vertical,
-		},
-		margins = 10,
-		widget = wibox.container.margin,
-	},
+	widget = wibox.layout.fixed.vertical,
 	border_color = beautiful.border_color,
 	border_width = 2,
 	placement = awful.placement.centered,
@@ -28,13 +14,35 @@ local todo_widget = awful.popup({
 	visible = false,
 })
 
---Toggle the widget on and off
-local function toggle_todo_widget()
-	todo_widget.visible = not todo_widget.visible
+local function read_tasks_from_file()
+	local tasks = dofile("test.lua")
+	return tasks
 end
 
---Get todos from list
-local function read_table() end
+local function update_todo_widget(tasks)
+	todo_widget.widget:reset()
+	--add title
+	todo_widget.widget:add(wibox.widget({
+		text = "TODO List:",
+		widget = wibox.widget.textbox,
+	}))
+
+	-- add the tasks from the datafile to the widget
+	for i, task in ipairs(tasks) do
+		local task_text = string.format("%d. %s", i, task.task)
+		todo_widget.widget:add(wibox.widget({
+			text = task_text,
+			widget = wibox.widget.textbox,
+		}))
+	end
+end
+
+--Toggle the widget on and off
+local function toggle_todo_widget()
+	local tasks = read_tasks_from_file()
+	update_todo_widget(tasks)
+	todo_widget.visible = not todo_widget.visible
+end
 
 return {
 	toggle_todo_widget = toggle_todo_widget,
